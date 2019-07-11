386c5da396eae80ca16c3492d49f844fc5b87566
Andrea Flores <andrea94flores@gmail.com>
2019-07-11 09:05:21 -0500

    adds login and signup forms

---
+++ controllers/users.js
@@ -0,0 +1,16 @@
+//jshint esversion:8
+const User = require('../models/user');
+
+module.exports = {
+    signup
+};
+
+async function signup(req, res) {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
--- models/User.js
+++ models/User.js
@@ -3,11 +3,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     name: String,
+    email: {type: String, required: true, lowercase: true, unique: true},
+    password: String,
     organization: String,
     subOrganization: String,
-    registeredVoter: Boolean, 
+    registeredVoter: Boolean
+}, {
+    timestamps: true
+});
+
+//removes the password when serializing doc to JSON 
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+        delete ret.password;
+        return ret;
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
